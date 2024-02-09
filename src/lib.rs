62aa78c5d1dcfae9a20b7534c79514c09bb10f53
wioenena-q <wioenena@gmail.com>
2024-02-09 21:35:20 +0300

    chore: macros moved to macros.rs and some name changing

--- src/lib.rs
+++ src/lib.rs
@@ -1,109 +1,106 @@
-macro_rules! define_c_type {
-    ($name:ident, $type:ty) => {
-        pub type $name = $type;
-    };
-}
+mod macros;
 
-macro_rules! define_constant {
-    ($name:ident, $type:ty, $value:expr) => {
-        pub const $name: $type = $value;
-    };
-}
-
+#[allow(non_camel_case_types)]
 pub mod core {
-    #[allow(non_camel_case_types)]
-    pub mod types {
-        define_c_type!(char_t, i8);
-        define_c_type!(u_char_t, u8);
-        define_c_type!(short_t, i16);
-        define_c_type!(u_short_t, u16);
-        #[cfg(target_pointer_width = "16")]
-        define_c_type!(int_t, i16);
-        #[cfg(target_pointer_width = "16")]
-        define_c_type!(u_int_t, u16);
-        #[cfg(any(target_pointer_width = "64", target_pointer_width = "32"))]
-        define_c_type!(int_t, i32);
-        #[cfg(any(target_pointer_width = "64", target_pointer_width = "32"))]
-        define_c_type!(u_int_t, u32);
-        #[cfg(target_pointer_width = "32")]
-        define_c_type!(long_t, i32);
-        #[cfg(target_pointer_width = "32")]
-        define_c_type!(u_long_t, u32);
-        #[cfg(target_pointer_width = "64")]
-        define_c_type!(long_t, i64);
-        #[cfg(target_pointer_width = "64")]
-        define_c_type!(u_long_t, u64);
-        define_c_type!(float_t, f32);
-        define_c_type!(double_t, f64);
-        define_c_type!(int_max_t, self::int_t);
-        define_c_type!(size_t, self::u_long_t);
-        define_c_type!(ssize_t, self::long_t);
-        define_c_type!(ptrdiff_t, self::long_t);
-        pub enum void {}
+    use crate::def_type;
 
-        pub mod ptr {
-            define_c_type!(void_ptr_t, *const super::void);
-            define_c_type!(void_ptr_t_mut, *mut super::void);
-            define_c_type!(nullptr_t, *const super::void);
-            define_c_type!(nullptr_mut_t, *mut super::void);
-        }
+    def_type!(char_t, i8);
+    def_type!(u_char_t, u8);
+    def_type!(short_t, i16);
+    def_type!(u_short_t, u16);
+    #[cfg(target_pointer_width = "16")]
+    def_type!(int_t, i16);
+    #[cfg(target_pointer_width = "16")]
+    def_type!(u_int_t, u16);
+    #[cfg(any(target_pointer_width = "64", target_pointer_width = "32"))]
+    def_type!(int_t, i32);
+    #[cfg(any(target_pointer_width = "64", target_pointer_width = "32"))]
+    def_type!(u_int_t, u32);
+    #[cfg(target_pointer_width = "32")]
+    def_type!(long_t, i32);
+    #[cfg(target_pointer_width = "32")]
+    def_type!(u_long_t, u32);
+    #[cfg(target_pointer_width = "64")]
+    def_type!(long_t, i64);
+    #[cfg(target_pointer_width = "64")]
+    def_type!(u_long_t, u64);
+    def_type!(float_t, f32);
+    def_type!(double_t, f64);
+    def_type!(int_max_t, self::int_t);
+    def_type!(size_t, self::u_long_t);
+    def_type!(ssize_t, self::long_t);
+    def_type!(ptrdiff_t, self::long_t);
+    def_type!(wchar16_t, u16);
+    def_type!(wchar32_t, u32);
+    pub enum void {}
+
+    pub mod ptr {
+        use crate::def_type;
+
+        def_type!(void_ptr_t, *const super::void);
+        def_type!(void_ptr_t_mut, *mut super::void);
+        def_type!(nullptr_t, *const super::void);
+        def_type!(nullptr_mut_t, *mut super::void);
     }
 
     #[allow(non_upper_case_globals)]
     pub mod constants {
-        define_constant!(CHAR_MIN, i8, i8::MIN);
-        define_constant!(CHAR_MAX, i8, i8::MAX);
-        define_constant!(UCHAR_MIN, u8, u8::MIN);
-        define_constant!(UCHAR_MAX, u8, u8::MAX);
-        define_constant!(INT_MIN, super::types::int_t, super::types::int_t::MIN);
-        define_constant!(INT_MAX, super::types::int_t, super::types::int_t::MAX);
-        define_constant!(UINT_MIN, super::types::u_int_t, super::types::u_int_t::MIN);
-        define_constant!(UINT_MAX, super::types::u_int_t, super::types::u_int_t::MAX);
-        define_constant!(LONG_MIN, super::types::long_t, super::types::long_t::MIN);
-        define_constant!(LONG_MAX, super::types::long_t, super::types::long_t::MAX);
-        define_constant!(
+        use crate::def_constant;
+
+        def_constant!(CHAR_MIN, i8, i8::MIN);
+        def_constant!(CHAR_MAX, i8, i8::MAX);
+        def_constant!(UCHAR_MIN, u8, u8::MIN);
+        def_constant!(UCHAR_MAX, u8, u8::MAX);
+        def_constant!(INT_MIN, super::int_t, super::int_t::MIN);
+        def_constant!(INT_MAX, super::int_t, super::int_t::MAX);
+        def_constant!(UINT_MIN, super::u_int_t, super::u_int_t::MIN);
+        def_constant!(UINT_MAX, super::u_int_t, super::u_int_t::MAX);
+        def_constant!(LONG_MIN, super::long_t, super::long_t::MIN);
+        def_constant!(LONG_MAX, super::long_t, super::long_t::MAX);
+        def_constant!(
             ULONG_MIN,
-            super::types::u_long_t,
-            super::types::u_long_t::MIN
+            super::u_long_t,
+            super::u_long_t::MIN
         );
-        define_constant!(
+        def_constant!(
             ULONG_MAX,
-            super::types::u_long_t,
-            super::types::u_long_t::MAX
+            super::u_long_t,
+            super::u_long_t::MAX
         );
-        define_constant!(SHORT_MIN, super::types::short_t, super::types::short_t::MIN);
-        define_constant!(SHORT_MAX, super::types::short_t, super::types::short_t::MAX);
-        define_constant!(
+        def_constant!(SHORT_MIN, super::short_t, super::short_t::MIN);
+        def_constant!(SHORT_MAX, super::short_t, super::short_t::MAX);
+        def_constant!(
             USHORT_MIN,
-            super::types::u_short_t,
-            super::types::u_short_t::MIN
+            super::u_short_t,
+            super::u_short_t::MIN
         );
-        define_constant!(
+        def_constant!(
             USHORT_MAX,
-            super::types::u_short_t,
-            super::types::u_short_t::MAX
+            super::u_short_t,
+            super::u_short_t::MAX
         );
-        define_constant!(FLOAT_MIN, super::types::float_t, super::types::float_t::MIN);
-        define_constant!(FLOAT_MAX, super::types::float_t, super::types::float_t::MAX);
-        define_constant!(
+        def_constant!(FLOAT_MIN, super::float_t, super::float_t::MIN);
+        def_constant!(FLOAT_MAX, super::float_t, super::float_t::MAX);
+        def_constant!(
             DOUBLE_MIN,
-            super::types::double_t,
-            super::types::double_t::MIN
+            super::double_t,
+            super::double_t::MIN
         );
-        define_constant!(
+        def_constant!(
             DOUBLE_MAX,
-            super::types::double_t,
-            super::types::double_t::MAX
+            super::double_t,
+            super::double_t::MAX
         );
-        define_constant!(TRUE, u8, 1);
-        define_constant!(FALSE, u8, 0);
-        define_constant!(NULL, u8, 0);
-        define_constant!(
+        def_constant!(TRUE, u8, 1);
+        def_constant!(FALSE, u8, 0);
+        def_constant!(NULL, u8, 0);
+        def_constant!(
             nullptr,
-            super::types::ptr::nullptr_t,
-            0 as super::types::ptr::nullptr_t
+            super::ptr::nullptr_t,
+            0 as super::ptr::nullptr_t
         );
     }
 }
 
+#[allow(non_camel_case_types)]
 pub mod win_types;
